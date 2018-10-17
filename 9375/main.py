e7c0e350b9fcee311af7365857c61dcdbd582bcd
Yeongho Kim <yeonghoey@gmail.com>
2018-10-18 04:01:58 +0900

    Refactor 9375

--- 9375/main.py
+++ 9375/main.py
@@ -1,17 +1,10 @@
 from collections import Counter
-from itertools import combinations
+from functools import reduce
 
 t = int(input())
 while t > 0:
     n = int(input())
-    wears = Counter()
-    for _ in range(n):
-        _, kind = input().split()
-        wears[kind] += 1
-
-    x = 1
-    for v in wears.values():
-        x *= v+1
+    wears = Counter(input().split()[1] for _ in range(n))
+    x = reduce(lambda v, e: v*(e+1), wears.values(), 1)
     print(x-1)
-
     t -= 1
