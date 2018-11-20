664b0f07b1d76ffeb4794dcefc41871684ace78e
Yeongho Kim <yeonghoey@gmail.com>
2018-11-21 08:59:35 +0900

    Solve 11726

---
+++ 11726/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	_, _ = fmt.Scan(&n)
+
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-2] + dp[i-1]
+		dp[i] %= 10007
+	}
+	fmt.Println(dp[n])
+}
