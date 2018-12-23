fe889d89f4c6455934d4af5fb2390a0390d61004
Yeongho Kim <yeonghoey@gmail.com>
2018-12-23 09:44:29 +0900

    Solve 1015

---
+++ 1015/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	scanner.Scan()
+	x := 0
+	for _, b := range scanner.Bytes() {
+		x *= 10
+		x += int(b - '0')
+	}
+	return x
+}
+
+type elem struct {
+	value int
+	index int
+}
+
+type withIndex []elem
+
+func (a withIndex) Len() int {
+	return len(a)
+}
+
+func (a withIndex) Less(i, j int) bool {
+	return a[i].value < a[j].value
+}
+
+func (a withIndex) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func main() {
+	N := nextInt()
+	A := make([]elem, N)
+	for i := range A {
+		A[i].value = nextInt()
+		A[i].index = i
+	}
+
+	sort.Stable(withIndex(A))
+
+	pMap := make(map[int]int)
+	for i, e := range A {
+		pMap[e.index] = i
+	}
+
+	P := make([]int, N)
+	for i := range P {
+		P[i] = pMap[i]
+	}
+
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fprintf(writer, "%d", P[0])
+	for i := 1; i < N; i++ {
+		fmt.Fprintf(writer, " %d", P[i])
+	}
+	_ = writer.Flush()
+}
