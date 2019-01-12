7f70bb7369d805f68604ef443661c0f8fde05bb8
Yeongho Kim <yeonghoey@gmail.com>
2019-01-12 11:31:33 -0600

    Add runner

---
+++ runner/runner.go
@@ -0,0 +1,68 @@
+package runner
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+// Runner keeps the context of running a command.
+type Runner struct {
+	t    *testing.T
+	name string
+	args []string
+}
+
+// New returns an instance of Runner
+func New(t *testing.T, name string, arg ...string) *Runner {
+	return &Runner{t, name, arg}
+}
+
+// Result represents the context of the execution.
+type Result struct {
+	t     *testing.T
+	input string
+	got   string
+}
+
+// Run executes "go run ." with input as stdin and
+// compares the output with the content of wantOutputName.
+func (r *Runner) Run(input string) *Result {
+	cmd := exec.Command("go", "run", ".")
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = f.Close() }()
+	cmd.Stdin = f
+
+	var buffer bytes.Buffer
+	cmd.Stdout = &buffer
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output := buffer.String()
+	got := strings.TrimSpace(output)
+	return &Result{r.t, input, got}
+}
+
+// WantFile tests the result against the content of wantOutputName.
+func (r *Result) Want(wantOutputName string) {
+	content, err := ioutil.ReadFile(wantOutputName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	want := string(content)
+	want = strings.TrimSpace(want)
+	if r.got != want {
+		r.t.Errorf("Input: %s, got %s, want %s", r.input, r.got, want)
+	}
+}
