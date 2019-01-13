916eef40556458bdbcecf04f3bd8d84f05be8a0a
Yeongho Kim <yeonghoey@gmail.com>
2019-01-13 10:23:16 -0600

    Use boj/runner instead of cmdtest

--- 14501/main_test.go
+++ 14501/main_test.go
@@ -3,31 +3,13 @@ package main
 import (
 	"testing"
 
-	"github.com/yeonghoey/cmdtest"
+	"github.com/yeonghoey/boj/runner"
 )
 
 func TestSamples(t *testing.T) {
-	cmd := cmdtest.Command("go", "run", ".")
-
-	samples := []struct {
-		input  string
-		output string
-	}{
-		{"input1.txt", "output1.txt"},
-		{"input2.txt", "output2.txt"},
-		{"input3.txt", "output3.txt"},
-		{"input4.txt", "output4.txt"},
-	}
-
-	for _, sa := range samples {
-		got, want, err := cmd.Run(sa.input, sa.output)
-
-		if err != nil {
-			t.Errorf("cmd.Run failed: %v", err)
-		}
-		if got != want {
-			t.Errorf("cmd.Run(%s) = %s, want %s",
-				sa.input, got, want)
-		}
-	}
+	r := runner.New(t, "go", "run", ".")
+	r.Run("input1.txt").Want("output1.txt")
+	r.Run("input2.txt").Want("output2.txt")
+	r.Run("input3.txt").Want("output3.txt")
+	r.Run("input4.txt").Want("output4.txt")
 }
--- 15486/main_test.go
+++ 15486/main_test.go
@@ -3,28 +3,10 @@ package main
 import (
 	"testing"
 
-	"github.com/yeonghoey/cmdtest"
+	"github.com/yeonghoey/boj/runner"
 )
 
 func TestSamples(t *testing.T) {
-	cmd := cmdtest.Command("go", "run", ".")
-
-	samples := []struct {
-		input  string
-		output string
-	}{
-		{"input1.txt", "output1.txt"},
-	}
-
-	for _, sa := range samples {
-		got, want, err := cmd.Run(sa.input, sa.output)
-
-		if err != nil {
-			t.Errorf("cmd.Run failed: %v", err)
-		}
-		if got != want {
-			t.Errorf("cmd.Run(%s) = %s, want %s",
-				sa.input, got, want)
-		}
-	}
+	r := runner.New(t, "go", "run", ".")
+	r.Run("input1.txt").Want("output1.txt")
 }
