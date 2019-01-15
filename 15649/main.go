1d5b9e65844b40946f3fec6cfefb876234278a26
Yeongho Kim <yeonghoey@gmail.com>
2019-01-15 06:05:12 -0800

    Solve 15649

---
+++ 15649/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	scanner.Scan()
+	sign, x := 1, 0
+	for _, b := range scanner.Bytes() {
+		if b == '-' {
+			sign = -1
+			continue
+		}
+		x *= 10
+		x += int(b - '0')
+	}
+	return sign * x
+}
+
+func main() {
+	N := nextInt()
+	M := nextInt()
+
+	A := make([]int, M+1)
+	B := make([]bool, N+1)
+
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var f func(int)
+	f = func(m int) {
+		if m > M {
+			s := strings.Trim(fmt.Sprint(A[1:]), "[]")
+			fmt.Fprintln(writer, s)
+			return
+		}
+		for x := 1; x <= N; x++ {
+			if B[x] {
+				continue
+			}
+			A[m] = x
+			B[x] = true
+			f(m + 1)
+			B[x] = false
+		}
+	}
+	f(1)
+}
---
+++ 15649/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/yeonghoey/boj/runner"
+)
+
+func TestSamples(t *testing.T) {
+	r := runner.New(t, "go", "run", ".")
+	r.Run("input1.txt").Want("output1.txt")
+	r.Run("input2.txt").Want("output2.txt")
+}
