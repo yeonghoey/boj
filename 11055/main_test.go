248c9ef9b26d5f68415c2345ca0017772c3929a1
Yeongho Kim <yeonghoey@gmail.com>
2019-01-21 20:38:19 +0900

    Solve 11055

---
+++ 11055/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/yeonghoey/boj/runner"
+)
+
+func TestSamples(t *testing.T) {
+	r := runner.New(t, "go", "run", ".")
+	samples := []struct {
+		intput, output string
+	}{
+		{"sample-input-1.txt", "sample-output-1.txt"},
+	}
+	for _, s := range samples {
+		r.Run(s.intput).Want(s.output)
+	}
+}
