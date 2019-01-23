77f6f474b06d6209f4e006bdbc146b190f344e54
Yeongho Kim <yeonghoey@gmail.com>
2019-01-24 06:32:49 +0900

    Improve 11653

--- 11653/main.go
+++ 11653/main.go
@@ -7,24 +7,25 @@ import (
 )
 
 func main() {
-	var N int
-	_, _ = fmt.Scan(&N)
+	var x int
+	_, _ = fmt.Scan(&x)
 
-	x := N
 	factors := make([]int, 0)
-	isComposite := make([]bool, N+1)
-	for p := 2; p <= N; p++ {
-		if !isComposite[p] {
-			for x%p == 0 {
-				factors = append(factors, p)
-				x /= p
-			}
-
-			for y := p + p; y <= x; y += p {
-				isComposite[y] = true
-			}
+	f := func(p int) {
+		for x%p == 0 {
+			factors = append(factors, p)
+			x /= p
 		}
 	}
+
+	f(2)
+	for p := 3; p*p <= x; p += 2 {
+		f(p)
+	}
+	if x > 1 {
+		factors = append(factors, x)
+	}
+
 	writer := bufio.NewWriter(os.Stdout)
 	defer func() { _ = writer.Flush() }()
 	for _, p := range factors {
