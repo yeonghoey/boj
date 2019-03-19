70147b57b3bb3e5e608e481ec54550770a032d67
Yeongho Kim <yeonghoey@gmail.com>
2019-03-20 07:49:30 +0900

    Solve 10797

---
+++ 10797/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	scanner.Scan()
+	sign, x := 1, 0
+	for _, b := range scanner.Bytes() {
+		if b == '-' {
+			sign = -1
+			continue
+		}
+		x *= 10
+		x += int(b - '0')
+	}
+	return sign * x
+}
+
+func main() {
+	X := nextInt()
+	count := 0
+	for i := 0; i < 5; i++ {
+		y := nextInt()
+		if y%10 == X {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
