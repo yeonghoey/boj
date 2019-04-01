f8b3bdcec908142ffc2aa81c6c4f51555174925c
Yeongho Kim <yeonghoey@gmail.com>
2019-04-02 05:56:05 +0900

    Solve 2475

---
+++ 2475/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	scanner.Scan()
+	sign, x := 1, 0
+	for _, b := range scanner.Bytes() {
+		if b == '-' {
+			sign = -1
+			continue
+		}
+		x *= 10
+		x += int(b - '0')
+	}
+	return sign * x
+}
+
+func main() {
+	a := nextInt()
+	b := nextInt()
+	c := nextInt()
+	d := nextInt()
+	e := nextInt()
+	fmt.Println((a*a + b*b + c*c + d*d + e*e) % 10)
+}
